a4d00b9c493c5789d3e99bbf0aaaeb4e3db19aee
Bharat Panchal <bharatpanchal3578@gmail.com>
2025-12-02 13:16:54 +0530

    feat: Add Task model with status and optional habit link

--- Habit_Tracker/tasks/models.py
+++ Habit_Tracker/tasks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('pending','Pending'),
+        ('done','Done'),
+    ]
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='tasks')
+    habit = models.ForeignKey("habits.Habit",on_delete=models.CASCADE, related_name="tasks", null=True, blank=True)
+    description = models.TextField()
+    date = models.DateField(default=timezone.now)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+
+    def __str__(self):
+        return f"{self.description} - {self.date}"
